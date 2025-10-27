24a127bb32b85902294f1dec57f9e283241b458e
vitacvv <dohasmsmh@gmail.com>
2025-10-27 12:42:47 +0100

    Create hello.js

---
+++ pages/hello.js
@@ -0,0 +1,8 @@
+export default function Hello() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Hello from VitaCV!</h1>
+      <p>If you can see this, your setup works perfectly.</p>
+    </div>
+  );
+}
