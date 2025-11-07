165ac67975c21f76cfb7537b77a5e35e2500a393
vitacvv <dohasmsmh@gmail.com>
2025-11-07 12:23:50 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css';
+// pages/_app.js
+import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
