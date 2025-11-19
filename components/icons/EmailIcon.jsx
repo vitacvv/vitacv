f01102d6a49d6cb3620e8fae42d1f0d5dff162d1
vitacvv <dohasmsmh@gmail.com>
2025-11-19 13:22:17 +0100

    Create EmailIcon.jsx

---
+++ components/icons/EmailIcon.jsx
@@ -0,0 +1,7 @@
+export default function EmailIcon() {
+  return (
+    <svg width="16" height="16" fill="currentColor" viewBox="0 0 24 24">
+      <path d="M2 4h20v16H2V4zm10 7L3.5 6h17L12 11z" />
+    </svg>
+  );
+}
