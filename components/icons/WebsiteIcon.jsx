011b6abd05c550ec00b38efff3773cf385d6f72f
vitacvv <dohasmsmh@gmail.com>
2025-11-19 13:31:11 +0100

    Create WebsiteIcon.jsx

---
+++ components/icons/WebsiteIcon.jsx
@@ -0,0 +1,7 @@
+export default function WebsiteIcon() {
+  return (
+    <svg width="16" height="16" fill="currentColor" viewBox="0 0 24 24">
+      <path d="M12 2a10 10 0 100 20 10 10 0 000-20zm0 3c1.1 0 2 .9 2 2v2h-4V7c0-1.1.9-2 2-2zm0 14a7 7 0 01-7-7h3a4 4 0 004 4v3zm0-7v1a3 3 0 01-3-3H6a6 6 0 006 6v-3a3 3 0 01-3-3h3z" />
+    </svg>
+  );
+}
