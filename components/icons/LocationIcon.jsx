25d74df9ed446e07887ab477e638c940ffee925c
vitacvv <dohasmsmh@gmail.com>
2025-11-19 13:30:53 +0100

    Create LocationIcon.jsx

---
+++ components/icons/LocationIcon.jsx
@@ -0,0 +1,7 @@
+export default function LocationIcon() {
+  return (
+    <svg width="16" height="16" fill="currentColor" viewBox="0 0 24 24">
+      <path d="M12 2a7 7 0 017 7c0 5.3-7 13-7 13S5 14.3 5 9a7 7 0 017-7zm0 9.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5z" />
+    </svg>
+  );
+}
